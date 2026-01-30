3ba137cad28dc46a031f90c060d80142c8e4af6f
sowbarnika <23suca54@tcarts.in>
2026-01-30 16:12:42 +0530

    intentation added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import MedicineForm from './components/MedicineForm';
 import StockList from './components/StockList';
 import MedicineService from './services/MedicineService';
-import 'react-toastify/dist/ReactToastify.css';
+     import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
       const [medicines, setMedicines] = useState([]);
